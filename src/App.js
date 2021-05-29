b2cd1cee66ddc0b606d10f6555d1bdff40b7c85b
Manikanta <manikanta.chavvakula@outlook.com>
2021-05-29 18:31:51 +0530

    Added Sidebar component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/pages/Home'
 function App() {
   return (
     <Home />
-  );
+  )
 }
 
 export default App;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import './Home.css';
 import LineChart from '../charts/LineChart'
+import {SidebarData} from './SidebarData'
+
 
 function Home() {
   return (
     <div class="container">
       <nav>Navbar</nav>
       <div id="main-content">
-        <div id="sidebar">Sidebar</div>
+      <div className="sidebar">
+        <ul className="SidebarList" >
+          {SidebarData.map((val, key)=>{
+            return (
+            <li key = {key}
+            className = "row"
+            id={window.location.pathname == val.link ? "active" : ""}
+            onClick={() => {window.location.pathname = val.link}}>
+                {" "}
+                <div id= "icon">{val.icon}</div>{" "}
+                <div id = "title">{val.title}</div>{" "}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
         <div id="inner-content">
           <div id="map">Map</div>
           <div id="summary">Summary</div>

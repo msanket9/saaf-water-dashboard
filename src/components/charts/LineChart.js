aea6f3303281bedfb98892e564d737ca156ad03f
c0deb1ind31 <rhutikparab1@gmail.com>
2021-05-31 14:30:46 +0530

    graph fix

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -1,56 +1,41 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-const LineChart = () => {
+const LineChart = ({ Graphdata }) => {
   const data = (canvas) => {
     const ctx = canvas.getContext("2d");
     const gradient = ctx.createLinearGradient(0, 0, 0, 400);
     gradient.addColorStop(0, "rgb(0, 199, 79)");
     gradient.addColorStop(1, "rgba(250,174,50,0)");
-    return {
-      labels: ["1", "2", "20", "80", "10"],
-      datasets: [
-        {
-          label: "TDS",
-          fill:"start",
-          backgroundColor: gradient, 
-          borderColor: "rgb(0, 199, 79)",
-          borderWidth: 2,
-          pointColor: "#fff",
-          pointStrokeColor: "rgb(0, 199, 79)",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgb(0, 199, 79)",
-          data: [300,100, 56, 50, 68, 80]
-        }
-      ]
-    };
+
+    return Graphdata;
   };
 
-  return (
-    <div
-      className="lineChart"
-    >
+  return Graphdata ? (
+    <div className="lineChart">
       <Line
         options={{
           legend: {
             display: false,
-            position: "bottom"
+            position: "bottom",
           },
           tooltips: {
             callbacks: {
               label: (tooltipItem) =>
                 `${tooltipItem.yLabel}: ${tooltipItem.xLabel}`,
-              title: () => null
-            }
+              title: () => null,
+            },
           },
           responsive: true,
-          maintainAspectRatio: false
+          maintainAspectRatio: false,
         }}
         data={data}
         height={100}
         width={100}
       />
     </div>
+  ) : (
+    ""
   );
 };
 
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,12 +2,9 @@ import React from "react";
 import NavBar from "../NavBar";
 import SideBar from "../Sidebar";
 import "./Home.css";
-import LineChart from '../../charts/LineChart'
-import MapComponent from "../map/MapComponent"
-
-
+import LineChart from "../../charts/LineChart";
+import MapComponent from "../map/MapComponent";
 function Home() {
-
   function closeMessages() {
     const messages = document.getElementById("messages");
     messages.style.display = "none";
@@ -24,30 +21,125 @@ function Home() {
         </div>
         <div className="inner_content">
           <div className="message" id="messages">
-            <button className="messageButton" onClick={closeMessages} >Close me</button>
+            <button className="messageButton" onClick={closeMessages}>
+              Close me
+            </button>
           </div>
           <div className="top_content">
-            <div className="map"><MapComponent/></div>
+            <div className="map">
+              <MapComponent />
+            </div>
             <div className="summary">summary</div>
           </div>
-          <Graphs/>
-
+          <Graphs />
         </div>
       </div>
     </div>
   );
 }
 export function Graphs() {
+  const GraphData = [
+    {
+      labels: ["1", "2", "20", "80", "10"],
+      datasets: [
+        {
+          label: "TDS",
+          fill: "start",
+          borderColor: "rgb(0, 199, 79)",
+          backgroundColor: "rgb(0, 199, 79)",
+          borderWidth: 2,
+          pointColor: "#fff",
+          pointStrokeColor: "rgb(0, 199, 79)",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgb(0, 199, 79)",
+          data: [300, 100, 56, 50, 68, 80],
+        },
+      ],
+    },
+    {
+      labels: ["1", "2", "20", "80", "10"],
+      datasets: [
+        {
+          label: "TDS",
+          fill: "start",
+          borderColor: "rgb(0, 199, 79)",
+          backgroundColor: "rgb(0, 199, 79)",
+          borderWidth: 2,
+          pointColor: "#fff",
+          pointStrokeColor: "rgb(0, 199, 79)",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgb(0, 199, 79)",
+          data: [300, 100, 56, 50, 68, 80],
+        },
+      ],
+    },
+    {
+      labels: ["1", "2", "20", "80", "10"],
+      datasets: [
+        {
+          label: "TDS",
+          fill: "start",
+          borderColor: "rgb(0, 199, 79)",
+          backgroundColor: "rgb(0, 199, 79)",
+          borderWidth: 2,
+          pointColor: "#fff",
+          pointStrokeColor: "rgb(0, 199, 79)",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgb(0, 199, 79)",
+          data: [300, 100, 56, 50, 68, 80],
+        },
+      ],
+    },
+    {
+      labels: ["1", "2", "20", "80", "10"],
+      datasets: [
+        {
+          label: "TDS",
+          fill: "start",
+          borderColor: "rgb(0, 199, 79)",
+          backgroundColor: "rgb(0, 199, 79)",
+          borderWidth: 2,
+          pointColor: "#fff",
+          pointStrokeColor: "rgb(0, 199, 79)",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgb(0, 199, 79)",
+          data: [300, 100, 56, 50, 68, 80],
+        },
+      ],
+    },
+    {
+      labels: ["1", "2", "20", "80", "10"],
+      datasets: [
+        {
+          label: "TDS",
+          fill: "start",
+          borderColor: "rgb(0, 199, 79)",
+          backgroundColor: "rgb(0, 199, 79)",
+          borderWidth: 2,
+          pointColor: "#fff",
+          pointStrokeColor: "rgb(0, 199, 79)",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgb(0, 199, 79)",
+          data: [300, 100, 56, 50, 68, 80],
+        },
+      ],
+    },
+  ];
   return (
     <>
       <div className="graphs">
-        <div className="graph_div"> <LineChart /> </div>
-        <div className="graph_div"> <LineChart /> </div>
-        <div className="graph_div"> <LineChart /> </div>
-        <div className="graph_div"> <LineChart /> </div>
-        <div className="graph_div" id="last-graph"> <LineChart /> </div>
+        {GraphData.map((data, index) => {
+          return (
+            <div className="graph_div" key={index}>
+              <LineChart Graphdata={data} />
+            </div>
+          );
+        })}
       </div>
-      <div className="history"> <LineChart /> </div>
+      <div className="history">
+        {" "}
+        <LineChart Graphdata={GraphData[0]} />{" "}
+      </div>
     </>
   );
 }
